91de8615f1053a7b55c31119c20900d0477fe7f2
Musa V. Ahmed <mahme012@fiu.edu>
2015-10-03 01:33:22 -0400

    Freaking testing in eclipse but not otherwise.

--- src/main/java/com/weatherology/weatherology/forecast/Forecast.java
+++ src/main/java/com/weatherology/weatherology/forecast/Forecast.java
@@ -4,10 +4,34 @@ public class Forecast {
 	protected String name;
 	protected double temperature;
 	protected double humidity;
-	
+
 	public Forecast(String name, double temperature, double humidity) {
 		this.name = name;
 		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getTemperature() {
+		return this.temperature;
+	}
+	
+	public double getHumidity() {
+		return this.humidity;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	
+	public void setHumidity(double humidity) {
 		this.humidity = humidity;
 	}
 }
--- src/test/java/com/weatherology/weatherology/forecast/ForecastTest.java
+++ src/test/java/com/weatherology/weatherology/forecast/ForecastTest.java
@@ -1,14 +1,55 @@
-import com.weatherology.weatherology.forecast.Forecast;
+package com.weatherology.weatherology.forecast;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ForecastTest {
+	private Forecast forecast;
 
-    @Test
-    public void forecastTest() {
-        Forecast forecast = new Forecast("WeatherProvider", 273.15, 90.0);
+	@Before
+	public void setUp() throws Exception {
+		forecast = new Forecast("WeatherProvider", 273.15, 90.0);
+	}
 
-        assertTrue(true);
-    }
+	@After
+	public void tearDown() throws Exception {
+		forecast = null;
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals(forecast.getName(), "WeatherProvider");
+	}
+
+	@Test
+	public void testGetTemperature() {
+		assertEquals(forecast.getTemperature(), 273.15, 0.01);
+	}
+
+	@Test
+	public void testGetHumidity() {
+		assertEquals(forecast.getHumidity(), 90.0, 0.01);
+	}
+	
+	@Test
+	public void testSetName() {
+		forecast.setName("TestName");
+		assertEquals(forecast.getName(), "TestName");
+	}
+	
+	@Test
+	public void testSetTemperature() {
+		forecast.setTemperature(270.0);
+		assertEquals(forecast.getTemperature(), 270.0, 0.01);
+	}
+	
+	@Test
+	public void testSetHumidity() {
+		forecast.setHumidity(95.0);
+		assertEquals(forecast.getHumidity(), 95.0, 0.01);
+	}
+	
 }
